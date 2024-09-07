a83746bc69cb8857fe47edd1b793e5f7f389c561
Berint Moffett <berint.moffett@gmail.com>
2024-09-07 14:36:40 -0600

    58 current  2024-09-05 09:58:05  24.11.20240831.12228ff  6.6.48                          *

--- home-manager/modules/obs-studio.nix
+++ home-manager/modules/obs-studio.nix
@@ -3,7 +3,6 @@
     programs.obs-studio = {
         enable = true;
         plugins = with pkgs.obs-studio-plugins; [
-            wlrobs
             obs-background-removal
             obs-pipewire-audio-capture
         ];
