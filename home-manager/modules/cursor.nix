1cd28873fd9755a027bd2cbd635e7eb415413067
Berint Moffett <berint.moffett@gmail.com>
2024-09-13 17:38:19 -0600

    fix cursor -> adwaita <3

--- home-manager/modules/cursor.nix
+++ home-manager/modules/cursor.nix
@@ -1,24 +1,14 @@
 { config, pkgs, ... }:
 {
-    home.pointerCursor = 
-    let 
-      getFrom = url: hash: name: {
-          gtk.enable = true;
-          x11.enable = true;
-          name = name;
-          size = 48;
-          package = 
-            pkgs.runCommand "moveUp" {} ''
-              mkdir -p $out/share/icons
-              ln -s ${pkgs.fetchzip {
-                url = url;
-                hash = hash;
-              }} $out/share/icons/${name}
-          '';
-        };
-    in
-      getFrom 
-        "https://github.com/ful1e5/XCursor-pro/releases/download/v2.0.2/XCursor-Pro-Dark.tar.xz"
-        "sha256-x1rmWRGPXshOgcxTUXbhWTQHAO/BT6XVfE8SVLNFMk4="
-        "XCursor-Pro-Dark";
+    home.packages = [
+        pkgs.adwaita-icon-theme
+    ];
+
+    home.pointerCursor = {
+        gtk.enable = true;
+        x11.enable = true;
+        name = "Adwaita";
+        package = pkgs.adwaita-icon-theme;
+        size = 24;
+    };
 }
