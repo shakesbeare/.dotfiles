8302fe23b0e460762132bb2e4550e62e09e2e189
Berint Moffett <berint.moffett@gmail.com>
2024-09-18 11:11:25 -0600

    88 current  2024-09-18 11:11:09  24.11.20240910.1355a0c  6.6.50                          *

--- home-manager/nixos-dt.nix
+++ home-manager/nixos-dt.nix
@@ -6,6 +6,10 @@
         programs.home-manager.enable = true;
     # } 
 
+    home.packages = with pkgs; [
+        gnumake
+    ];
+
     home.sessionVariables = {
         SYSTEM = "x86_64-linux";
     };
