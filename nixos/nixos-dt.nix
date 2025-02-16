5acdee32318563061dfa9d236ef428b24cd40b4c
Berint Moffett <berint.moffett@gmail.com>
2025-02-16 13:31:28 -0700

    103 current  2025-02-16 13:31:23  25.05.20250215.8bb3716  6.12.13                          *

--- nixos/nixos-dt.nix
+++ nixos/nixos-dt.nix
@@ -40,6 +40,7 @@
 
     # Set your time zone.
     time.timeZone = "America/Boise";
+    time.hardwareClockInLocalTime = true;
 
     # Select internationalisation properties.
     i18n.defaultLocale = "en_US.UTF-8";
