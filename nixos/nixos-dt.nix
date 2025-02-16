f00198aac67454dc7311ea9393d52606fbe728d5
Berint Moffett <berint.moffett@gmail.com>
2025-02-16 13:29:26 -0700

    102 current  2025-02-16 13:29:20  25.05.20250215.8bb3716  6.12.13                          *

--- nixos/nixos-dt.nix
+++ nixos/nixos-dt.nix
@@ -18,6 +18,7 @@
     boot.loader.systemd-boot.enable = false;
 
     boot.loader.grub.enable = true;
+    boot.loader.timeout = null;
     boot.loader.grub.device = "nodev";
     boot.loader.grub.useOSProber = true;
     boot.loader.grub.efiSupport = true;
