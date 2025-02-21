d7b45706b60f5dcf5874efda6e7195892704da95
Berint Moffett <berint.moffett@gmail.com>
2025-02-21 11:29:19 -0700

    this is leftover after merge conflicts

--- home-manager/macos-lt.nix
+++ home-manager/macos-lt.nix
@@ -30,17 +30,23 @@
     SYSTEM = "aarch64-darwin";
   };
 
-  # Need to create aliases because Launchbar doesn't look through symlinks.
-  home.activation.link-apps = lib.hm.dag.entryAfter ["linkGeneration"] ''
-    new_nix_apps="${config.home.homeDirectory}/Applications/Nix"
-    rm -rf "$new_nix_apps"
-    mkdir -p "$new_nix_apps"
-    find -H -L "$genProfilePath/home-files/Applications" -name "*.app" -type d -print | while read -r app; do
-        real_app=$(readlink -f "$app")
-        app_name=$(basename "$app")
-        target_app="$new_nix_apps/$app_name"
-        echo "Alias '$real_app' to '$target_app'"
-        ${pkgs.mkalias}/bin/mkalias "$real_app" "$target_app"
-    done
-  '';
+    home.activation = if pkgs.stdenv.isDarwin then 
+    {
+        brewInstallFirefox = lib.hm.dag.entryAfter ["writeBoundary"] ''
+        /opt/homebrew/bin/brew install firefox 
+        '';
+        brewInstallDropbox = lib.hm.dag.entryAfter ["writeBoundary"] ''
+        /opt/homebrew/bin/brew install dropbox
+        '';
+        brewInstallMacsFanControl = lib.hm.dag.entryAfter ["writeBoundary"] ''
+        /opt/homebrew/bin/brew install macs-fan-control
+        '';
+        brewInstallSpotify = lib.hm.dag.entryAfter ["writeBoundary"] ''
+        /opt/homebrew/bin/brew install spotify
+        '';
+
+        brewInstallZig = lib.hm.dag.entryAfter ["writeBoundary"] ''
+        /opt/homebrew/bin/brew install zig
+        '';
+    } else {};
 }
