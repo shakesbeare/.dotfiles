7d92379c21d1da29c4d7404ba74e0cfc1e7ba3e6
Berint Moffett <berint.moffett@gmail.com>
2025-02-22 01:14:23 -0700

    116 current  2025-02-22 01:14:15  25.05.20250218.73cf49b  6.12.14                          *

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -1,14 +1,21 @@
 {
-  config,
-  pkgs,
   ...
 }: {
   programs.git = {
     enable = true;
     userName = "Berint Moffett";
     userEmail = "berint.moffett@gmail.com";
+    aliases = {
+        lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+        hist = "log --pretty=format:'%C(yellow)[%ad]%C(reset) %C(green)[%h]%C(reset) | %C(red)%s %C(bold red){{%an}}%C(reset) %C(blue)%d%C(reset)' --graph --date=short";
+        graph = "log --all --graph --decorate --pretty=format:'%C(#005f87)%h%Creset :%C(#d75f00)%d%Creset %C(#005f87)%an%Creset %C(#008700)%cd (%cr)%n%s%n' --date=short";
+    };
     extraConfig = {
       init.defaultBranchName = "main";
+      core.editor = "$(which nvim)";
+      "mergetool \"nvim\"" = {
+	cmd = "/Users/bmoffett/.local/share/bob/nvim-bin/nvim \\\"$MERGED\\\" -c \":Gvdiffsplit!\"";
+      };
     };
   };
 }
