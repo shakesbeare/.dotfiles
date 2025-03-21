752078d861997be7607f643bebce213d3f3565e9
Berint Moffett <berint.moffett@gmail.com>
2025-03-21 17:35:44 -0600

    123 current  2025-03-21 17:35:35  25.05.20250218.73cf49b  6.12.14                          *

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -1,37 +1,37 @@
 {...}: {
-    programs = {
-        jujutsu = {
-            enable = true;
-            settings = {
-                user = {
-                    email = "berint.moffett@gmail.com";
-                    name = "Berint Moffett";
-                };
-                ui = {
-                    paginate = "never";
-                    default-command = "log";
-                };
-            };
+  programs = {
+    jujutsu = {
+      enable = true;
+      settings = {
+        user = {
+          email = "berint.moffett@gmail.com";
+          name = "Berint Moffett";
         };
-        git = {
-            enable = true;
-            userName = "Berint Moffett";
-            userEmail = "berint.moffett@gmail.com";
-            aliases = {
-                lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-                hist = "log --pretty=format:'%C(yellow)[%ad]%C(reset) %C(green)[%h]%C(reset) | %C(red)%s %C(bold red){{%an}}%C(reset) %C(blue)%d%C(reset)' --graph --date=short";
-                graph = "log --all --graph --decorate --pretty=format:'%C(#005f87)%h%Creset :%C(#d75f00)%d%Creset %C(#005f87)%an%Creset %C(#008700)%cd (%cr)%n%s%n' --date=short";
-            };
-            extraConfig = {
-                init.defaultBranch = "main";
-                core.editor = "$(which nvim)";
-                merge = {
-                    tool = "nvim";
-                };
-                mergetool.nvim = {
-                    cmd = "nvim \\\"$MERGED\\\" -c \":Gvdiffsplit!\"";
-                };
-            };
+        ui = {
+          paginate = "never";
+          default-command = "log";
         };
+      };
     };
+    git = {
+      enable = true;
+      userName = "Berint Moffett";
+      userEmail = "berint.moffett@gmail.com";
+      aliases = {
+        lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+        hist = "log --pretty=format:'%C(yellow)[%ad]%C(reset) %C(green)[%h]%C(reset) | %C(red)%s %C(bold red){{%an}}%C(reset) %C(blue)%d%C(reset)' --graph --date=short";
+        graph = "log --all --graph --decorate --pretty=format:'%C(#005f87)%h%Creset :%C(#d75f00)%d%Creset %C(#005f87)%an%Creset %C(#008700)%cd (%cr)%n%s%n' --date=short";
+      };
+      extraConfig = {
+        init.defaultBranch = "main";
+        core.editor = "$(which nvim)";
+        merge = {
+          tool = "nvim";
+        };
+        mergetool.nvim = {
+          cmd = "nvim \\\"$MERGED\\\" -c \":Gvdiffsplit!\"";
+        };
+      };
+    };
+  };
 }
