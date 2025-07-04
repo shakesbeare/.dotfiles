b55d60eab5869eca5b6354af0fff13cb1e7ab4d8
Berint Moffett <berint.moffett@gmail.com>
2025-07-04 11:37:29 -0700

    152   2025-07-04 11:37:07   (current)

--- sketchybar/bar.lua
+++ sketchybar/bar.lua
@@ -3,6 +3,7 @@ local colors = require("colors")
 -- Equivalent to the --bar domain
 sbar.bar({
   height = 40,
+  notch_width = 200,
   color = colors.bar.bg,
   padding_right = 2,
   padding_left = 2,
