7420dc6a79f15772b621479801416d8a507e24cb
Berint Moffett <berint.moffett@gmail.com>
2026-01-06 10:47:31 -0800

    167   2026-01-06 10:47:24   (current)

--- home-manager/modules/neovim.nix
+++ home-manager/modules/neovim.nix
@@ -1,13 +1,17 @@
 {
   config,
   pkgs,
+  inputs,
   ...
 }: {
   home.sessionVariables = {
     EDITOR = "nvim";
   };
 
-  programs.neovim.enable = true;
+  programs.neovim = {
+    enable = true;
+    package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
+  };
   home.file = {
     ".config/nvim".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/nvim_config";
   };
