594ce43694aed45b72ad7b2ad578881f4971b13a
Berint Moffett <berint.moffett@gmail.com>
2026-04-11 00:14:59 -0700

    174   2026-04-11 00:14:52   (current)

--- home-manager/modules/zellij.nix
+++ home-manager/modules/zellij.nix
@@ -9,9 +9,5 @@
 
   home.file = {
     ".config/zellij/config.kdl".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/zellij/config.kdl";
-    ".config/zellij/plugins/zellij-sessionizer.wasm".source = pkgs.fetchurl {
-      url = "https://github.com/laperlej/zellij-sessionizer/releases/download/v0.5.0/zellij-sessionizer.wasm";
-      sha256 = "0nx01gmf9jaivw22dffdy8nj1v840mgdkn0gkbwq2kp74g042664";
-    };
   };
 }
