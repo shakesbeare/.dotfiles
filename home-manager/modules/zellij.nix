de7552d638dd07075e9d0bb827421f2ee58d4586
Berint Moffett <berint.moffett@gmail.com>
2026-04-12 13:10:36 -0700

    176   2026-04-12 13:09:58   (current)

--- home-manager/modules/zellij.nix
+++ home-manager/modules/zellij.nix
@@ -9,5 +9,6 @@
 
   home.file = {
     ".config/zellij/config.kdl".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/zellij/config.kdl";
+    ".config/zellij/layouts".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/zellij/layouts";
   };
 }
