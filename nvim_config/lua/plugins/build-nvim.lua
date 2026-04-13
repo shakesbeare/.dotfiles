40e07d2e366c6c01a63a6616d230768ea5647610
Berint Moffett <berint.moffett@gmail.com>
2026-04-13 14:56:25 -0700

    bump treesitter config and re-remove build-nvim

--- nvim_config/lua/plugins/build-nvim.lua
+++
@@ -1,4 +0,0 @@
-return {
-    "cyuria/build.nvim",
-    opts = {},
-}
--- nvim_config/lua/plugins/treesitter.lua
+++ nvim_config/lua/plugins/treesitter.lua
@@ -1,110 +1,117 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	event = "BufReadPre",
-	build = ":TSUpdate",
-	dependencies = {
-		{ "nvim-treesitter/nvim-treesitter-context" },
-		{ "nvim-treesitter/nvim-treesitter-textobjects" },
-	},
-	config = function(_, _)
-		require("nvim-treesitter.configs").setup({
-			ignore_install = { "phpdoc" },
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = { "markdown" },
-			},
-			indent = {
-				enable = true,
-			},
-			playground = {
-				enable = true,
-			},
-			rainbow = {
-				enable = true,
-			},
-			autotag = {
-				enable = true,
-				filetypes = {
-					"html",
-					"javascript",
-					"typescript",
-					"javascriptreact",
-					"typescriptreact",
-					"svelte",
-					"vue",
-					"tsx",
-					"jsx",
-					"rescript",
-					"xml",
-					"php",
-					"markdown",
-					"astro",
-					"glimmer",
-					"handlebars",
-					"hbs",
-					"rust",
-				},
-			},
-		})
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = "false",
+		branch = "main",
+		build = ":TSUpdate",
+		dependencies = {
+		},
+		config = function(_, _)
+			-- features desired:
+			--     highlight ✓
+			--     indent ✓
+			--     rainbow
+			--     autotag
 
-		require("treesitter-context").setup({
-			enable = true,
-			max_lines = 0,
-			trim_scope = "outer",
-			min_window_height = 0,
-			patterns = {
-				default = {
-					"class",
-					"function",
-					"method",
-					"while",
-					"for",
-					"if",
-					"switch",
-					"case",
-				},
-				rust = {
-					"impl_item",
-					"struct",
-					"enum",
-				},
-				scala = {
-					"object_definition",
-				},
-				vhdl = {
-					"process_statement",
-					"architecture_body",
-					"entity_declaration",
-				},
-				markdown = {
-					"section",
-				},
-				elixir = {
-					"anonymous_function",
-					"arguments",
-					"block",
-					"do_block",
-					"list",
-					"map",
-					"tuple",
-					"quoted_content",
-				},
-				json = {
-					"pair",
-				},
-				yaml = {
-					"block_mapping_pair",
-				},
-			},
-			exact_patterns = {
-				-- Example for a specific filetype with Lua patterns
-				-- Treat patterns.rust as a Lua pattern (i.e "^impl_item$" will
-				-- exactly match "impl_item" only)
-				-- rust = true,
-			},
-			zindex = 20,
-			mode = "cursor",
-			separator = nil,
-		})
-	end,
+			-- configure nvim-treesitter
+			vim.api.nvim_create_autocmd("FileType", {
+				group = vim.api.nvim_create_augroup("treesitter.setup", {}),
+				callback = function(args)
+					local buf = args.buf
+					local filetype = args.match
+
+					-- Prevent treesitter from trying to enable on things like oil.nvim buffers
+					local language = vim.treesitter.language.get_lang(filetype) or filetype
+					if not vim.treesitter.language.add(language) then 
+						return
+					end
+					-- enable highlights
+					vim.treesitter.start() 
+					-- enable folding
+					vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+					vim.wo[0][0].foldmethod = "expr"
+					-- enable indent
+					vim.bo.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
+				end,
+			})
+		end,
+	},
+	{ 
+		"nvim-treesitter/nvim-treesitter-context", 
+		branch = "master", 
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function() 
+			-- enable context
+			require("treesitter-context").setup({
+				enable = true,
+				max_lines = 0,
+				trim_scope = "outer",
+				min_window_height = 0,
+				multiline_threshold = 20,
+				zindex = 20,
+				mode = "cursor",
+				separator = nil,
+			})
+		end
+	},
+	{ 
+		"nvim-treesitter/nvim-treesitter-textobjects", 
+		branch = "main",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function() 
+			require("nvim-treesitter-textobjects").setup({
+				
+			})
+			-- https://github.com/nvim-treesitter/nvim-treesitter-textobjects/blob/main/BUILTIN_TEXTOBJECTS.md
+			local to_select = require("nvim-treesitter-textobjects.select")
+			local to_move = require("nvim-treesitter-textobjects.move")
+			local ts_repeat_move = require("nvim-treesitter-textobjects.repeatable_move")
+
+			-- selection texto_selectbjects
+			vim.keymap.set({ "x", "o" }, "af", function() 
+				to_select.select_textobject("@function.outer", "textobjects")
+			end)
+
+			vim.keymap.set({ "x", "o" }, "if", function() 
+				to_select.select_textobject("@function.inner", "textobjects")
+			end)
+
+			-- movements
+			vim.keymap.set({ "n", "x", "o" }, "]f", function()
+				to_move.goto_next_start("@function.outer", "textobjects")
+			end)
+
+			vim.keymap.set({ "n", "x", "o" }, "[f", function()
+				to_move.goto_previous_start("@function.outer", "textobjects")
+			end)
+
+			vim.keymap.set({ "n", "x", "o" }, "]c", function()
+				to_move.goto_next_start("@class.outer", "textobjects")
+			end)
+
+			vim.keymap.set({ "n", "x", "o" }, "[c", function()
+				to_move.goto_previous_start("@class.outer", "textobjects")
+			end)
+
+			vim.keymap.set({ "n", "x", "o" }, "]n", function() 
+				to_move.goto_next_start({"@class.outer", "@function.outer"}, "textobjects")
+			end)
+
+			vim.keymap.set({ "n", "x", "o" }, "[n", function() 
+				to_move.goto_next_start({"@class.outer", "@function.outer"}, "textobjects")
+			end)
+
+			-- enable ;/,
+			-- ; always goes in the same direction while , always moves opposite
+			vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+			vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+
+			-- make f/F and t/T repeatable
+			vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+			vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+			vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+			vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
+
+		end
+	},
 }
